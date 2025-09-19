fa671b8d768af13b4a8ca7d8b3ef95c1c740bf19
Riwal LeMoan <rlmd29.dz@gmail.com>
2025-09-19 12:01:25 +0200

    Code nettoyage données

--- Nettoyage_donnee.R
+++ Nettoyage_donnee.R
@@ -1,1 +1,7 @@
 #Nettoyage des données 
+library(dplyr)
+athletes <- read.csv("athlete_events.csv", header = TRUE, stringsAsFactors=TRUE)
+
+athletes <- athletes %>% filter(Year>1924) %>%
+  mutate(Medal = ifelse(is.na(Medal), "No_medal", Medal))%>% mutate(Medal=as.factor(Medal)) %>% na.omit()
+summary(athletes)
