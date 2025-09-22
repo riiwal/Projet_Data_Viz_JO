3ed09b8474a71bea739d683648edba2bf24879ea
Melina-cmt <melinaclement44@gmail.com>
2025-09-22 11:45:18 +0200

    Sélection des individus : France métropolitaine

--- Nettoyage_donnee.R
+++ Nettoyage_donnee.R
@@ -8,8 +8,12 @@ dta_insee[, -c(1,2)] <- lapply(dta_insee[, -c(1,2)], function(x) {
 })
 
 dta_insee[,c(1,2)] <- lapply(dta_insee[,c(1,2)],as.factor)
+
+# Selection des variables d'intérêts
 dta_insee<-dta_insee[,c(1:4,7:13,19:21,37:50,54:55,59:63,91:98,118:123)]
-names(dta_insee)
+
+# Selection des observations : France Métropolitaine
+dta_insee <- dta_insee[1:540,]
 
 summary(dta_insee)
 
