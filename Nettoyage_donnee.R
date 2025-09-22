85f7af96ceff4a934039c44350a5f386eefc7a56
Riwal LeMoan <rlmd29.dz@gmail.com>
2025-09-22 18:41:59 +0200

    importation du jeux de données avec les résultats de l'éléction 2022

--- Nettoyage_donnee.R
+++ Nettoyage_donnee.R
@@ -1,8 +1,10 @@
 library(readxl)
 library(dplyr)
+
 dta_insee <- read_xlsx("indic-stat-circonscriptions-legislatives-2022.xlsx",sheet = 1, skip = 7)
 summary(dta_insee)
 
+#On passe les données en numérique car l'importation ne s'est pas bien faites 
 dta_insee[, -c(1,2)] <- lapply(dta_insee[, -c(1,2)], function(x) {
   as.numeric(gsub(",", ".", gsub("[^0-9,.-]", "", x))) 
 })
@@ -13,7 +15,26 @@ dta_insee[,c(1,2)] <- lapply(dta_insee[,c(1,2)],as.factor)
 dta_insee<-dta_insee[,c(1:4,7:13,19:21,37:50,54:55,59:63,91:98,118:123)]
 
 # Selection des observations : France Métropolitaine
-dta_insee <- dta_insee[1:540,]
+dta_insee <- dta_insee[2:540,]
 
-summary(dta_insee)
+# on retire les espaces pour permettre la jointure des tables
+dta_insee$`Nom de la circonscription` <- gsub(" ", "", dta_insee$`Nom de la circonscription`)
 
+# on importe les données resultats
+result_2022 <- read_xlsx("resultats-par-niveau-cirlg-t1-france-entiere.xlsx",sheet = 1)
+
+# selection de la france métropolitaine
+result_2022 <- result_2022[1:539,] 
+result_2022$`Libellé du département` <- gsub(" ", "", result_2022$`Libellé du département`)
+# on créer la colonne qui servira pour faire la jointure
+result_2022 %>% 
+  mutate(`Nom de la circonscription` = ifelse(`Code de la circonscription`=="01",
+                                            paste0(`Libellé du département`, "-", "1recirconscription"),
+                                            paste0(paste0(`Libellé du département`, "-", as.numeric(`Code de la circonscription`),"ecirconscription"))))-> result_2022
+
+
+# on crée le jeu de données d'analyses
+
+donnees <- dta_insee %>%
+  full_join(result_2022, by = "Nom de la circonscription")
+summary(donnees)
