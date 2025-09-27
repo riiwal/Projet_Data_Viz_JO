1c8ab9a06577e1ca78d51beaeecace830a9cc39d
Maéva MONTIER <maeva.montier76@gmail.com>
2025-09-27 14:01:25 +0200

    nettoyage code

--- Exploration/Exploration_Maeva.R
+++ Exploration/Exploration_Maeva.R
@@ -19,9 +19,9 @@ str(dtaf)
 # ---- 1. ANALYSE UNIVARIÉE ----
 # Variables électorales principales
 num_vars <- c("Inscrit_22", "pop_légal_19", "age_moyen", "actemp", "actcho", "inactret",
-,    "actdip_PEU","actdip_CAP","actdip_BAC","actdip_BAC2","actdip_BAC3",
-,    "actdip_BAC5","proprio","locatai","mfuel","men_seul","tx_pauvrete60_diff",
-,    "nivvie_median_diff","% Voix/Ins3","% Voix/Ins5","% Voix/Ins7","% Voix/Ins8","% Voix/Ins9","% Voix/Ins10","% Voix/Ins11","% Voix/Ins12")
+              ,    "actdip_PEU","actdip_CAP","actdip_BAC","actdip_BAC2","actdip_BAC3",
+              ,    "actdip_BAC5","proprio","locatai","mfuel","men_seul","tx_pauvrete60_diff",
+              ,    "nivvie_median_diff","% Voix/Ins3","% Voix/Ins5","% Voix/Ins7","% Voix/Ins8","% Voix/Ins9","% Voix/Ins10","% Voix/Ins11","% Voix/Ins12")
 
 # Histogrammes et densités
 for (v in num_vars) {
@@ -226,16 +226,25 @@ fviz_pca_var(res.pca,repel = TRUE) +
 
 library(rpart)
 library(dplyr)
+library(visNetwork)
+library(rpart.plot)
+library(sparkline)
 
+data = dtaf %>%select(Inscrit_22,pop_légal_19,pop_pole_aav,pop_cour_aav,
+                      pop_horsaav,pop_urb,pop_rur_periu,pop_rur_non_periu,age_moyen,
+                      actemp,actcho,inactret,actdip_PEU,actdip_CAP,actdip_BAC,actdip_BAC2,
+                      actdip_BAC3,actdip_BAC5,actdip_BAC3P,
+                      act_agr,act_art,act_cad,act_int,act_emp,act_ouv,act_cho,
+                      men_seul,men_coupae,men_coupse,men_monop,proprio,locatai,mfuel,
+                      modtrans_aucun,modtrans_pied,modtrans_velo,modtrans_voit,modtrans_commun,
+                      acc_ecole,acc_college,acc_lycee,acc_medecin,acc_dentiste,acc_pharmacie,
+                      tx_pauvrete60_diff,nivvie_median_diff,Jadot_exp) %>%
+  st_drop_geometry()
 # Basic classification tree
-res <- rpart(Gagnant~., data = dtaf %>%select(Inscrit_22,pop_légal_19,pop_pole_aav,pop_cour_aav,pop_horsaav,pop_urb,pop_rur_periu,pop_rur_non_periu,age_moyen,
-             actemp,actcho,inactret,
-             actdip_PEU,actdip_CAP,actdip_BAC,actdip_BAC2,actdip_BAC3,actdip_BAC5,actdip_BAC3P,
-             act_agr,act_art,act_cad,act_int,act_emp,act_ouv,act_cho,
-             men_seul,men_coupea,men_coupse,men_monop,
-             proprio,locatai,mfuel,
-             modtrans_aucun,modtrans_pied,modtrans_velo,modtrans_voit,modtrans_commun,
-             acc_ecole,acc_college,acc_lycee,acc_medecin,acc_dentiste,acc_pharmacie,
-             tx_pauvrete60_diff,nivvie_median_diff,Gagnant) %>%
-            st_drop_geometry())
-visTree(res, main = "Vote", width = "100%")
+res <- rpart(Gagnant~., data = data)
+visTree(res, main = "Vote", width = "100%",height = "600px")
+
+res_jadot <- rpart(Jadot_exp~., data = data)
+visTree(res_jadot, main = "Vote", width = "100%",height = "600px")
+
+# renommer variables + ajuster ecriture
