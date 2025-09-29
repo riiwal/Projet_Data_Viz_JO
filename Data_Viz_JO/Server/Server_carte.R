2eb4406101c40a6c75c5341e78759bed7cdbe103
Riwal LeMoan <rlmd29.dz@gmail.com>
2025-09-29 20:21:22 +0200

    Carte Riwal premier jet

--- Data_Viz_JO/Server/Server_carte.R
+++ Data_Viz_JO/Server/Server_carte.R
@@ -3,107 +3,102 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(RColorBrewer)
-library("ggpubr")
-library("ggrepel")
+library(ggpubr)
+library(ggrepel)
 library(ggiraph)
 library(plotly)
 library(leaflet)
 library(leaflet.extras)
-library(htmltools) 
+library(htmltools)
 
-
-
-#On créer un subset avec seulement les votes et les circonscription
-top3<-dtaf_loaded[,c(3,56:67)] %>% 
-  pivot_longer(cols=2:13,
+# Préparation des données top3 (une seule fois, en dehors)
+top3 <- dtaf_loaded[, c(3, 56:67)] %>% 
+  pivot_longer(cols = 2:13,
                names_to = "candidat",
                values_to = "pourcentage")
 
 top3$candidat <- as.factor(top3$candidat)
 
-summary(top3)
-
-#la j'ai les 3 premiers 
-top3<-top3 %>% 
+# Garder les 3 premiers candidats par circonscription
+top3 <- top3 %>% 
   group_by(codeCirconscription) %>% 
   slice_max(pourcentage, n = 3, with_ties = FALSE)
 
+# Renommer les candidats
 top3 <- top3 %>%
   mutate(candidat = recode(candidat,
                            "Arthaud_exp" = "Arthaud",
                            "Roussel_exp" = "Roussel",
                            "Macron_exp" = "Macron",
                            "Lassalle_exp" = "Lasalle",
-                           "LePen_exp" = "LePen",
+                           "LePen_exp" = "Le Pen",
                            "Zemmour_exp" = "Zemmour",
-                           "Melenchon_exp" = "Melenchon",
+                           "Melenchon_exp" = "Mélenchon",
                            "Hidalgo_exp" = "Hidalgo",
                            "Jadot_exp" = "Jadot",
-                           "Pecresse_exp" = "Pecresse",
+                           "Pecresse_exp" = "Pécresse",
                            "Poutou_exp" = "Poutou",
-                           "DupontAignan_exp" = "DupontAignan"))
+                           "DupontAignan_exp" = "Dupont-Aignan"))
 
-
-pal <- colorNumeric("Oranges", domain = dtaf$act_cho)
-
-
-labels_top3 <- top3[,c(1,3,4)] %>%
+# Créer les labels top3 (une seule fois)
+labels_top3 <- top3 %>%
   group_by(codeCirconscription) %>%
-  summarise(top3_label = paste0(candidat, " : ", round(pourcentage, 1), "%", collapse = "<br>"))
-labels_top3 <- as_tibble(labels_top3)
+  summarise(top3_label = paste0(candidat, " : ", round(pourcentage, 1), "%", 
+                                collapse = "<br>"),
+            .groups = "drop")
 
-dtaf2 <- dtaf_loaded %>%
-  left_join(labels_top3, by = "codeCirconscription")
-
-
-######################################################
-
+# Render de la carte
 output$mymap <- renderLeaflet({
+  
+  # Joindre les données top3
+  dtaf2 <- dtaf_loaded %>%
+    left_join(st_drop_geometry(labels_top3), by = "codeCirconscription")
+  
+  # Créer la palette selon la sélection
+  pal <- colorNumeric("Oranges", domain = dtaf2[[input$select]])
+  
+  # Créer les labels HTML
+  labels <- lapply(
+    paste0(
+      "<b>", dtaf2$nomDepartement, " - ", dtaf2$nomCirconscription, "</b><br>",
+      "<b>", names(dtaf2)[which(names(dtaf2) == input$select)], " : </b>",
+      round(dtaf2[[input$select]], 2), "%<br>",
+      ifelse(!is.na(dtaf2$top3_label), 
+             paste0("<br><b>Top 3 :</b><br>", dtaf2$top3_label), 
+             "")
+    ),
+    HTML
+  )
+  
+  # Créer la carte Leaflet
   leaflet(dtaf2) %>%
+    addTiles() %>%
     addPolygons(
-      fillColor = ~pal(act_cho),
-      fillOpacity = 1,
+      fillColor = ~pal(dtaf2[[input$select]]),
+      fillOpacity = 0.7,
       color = "darkgray",
       weight = 1,
-      # Tooltip avec HTML
-      label = lapply(
-        paste0(
-          dtaf2$nomDepartement, " ", dtaf2$nomCirconscription, ": ", round(dtaf2$act_cho, 2), "%<br>",
-          ifelse(!is.na(dtaf2$top3_label), paste0("Top 3 :<br>", dtaf2$top3_label), "")
-        ),
-        HTML
+      opacity = 1,
+      highlightOptions = highlightOptions(
+        weight = 2,
+        color = "#333",
+        fillOpacity = 0.9,
+        bringToFront = TRUE
+      ),
+      label = labels,
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", padding = "3px 8px"),
+        textsize = "13px",
+        direction = "auto"
       )
-    ) %>% 
-    addTiles(
-      urlTemplate = "", 
-      options = providerTileOptions(minZoom = 5, maxZoom = 11)
-    ) %>% 
-    setMapWidgetStyle(style = list(background = "transparent"))
-  
-})
-#########################################################
-
-# Joindre avec dtaf
-
-
-
-leaflet(dtaf2) %>%
-    addPolygons(
-    fillColor = ~pal(act_cho),
-    fillOpacity = 1,
-    color = "darkgray",
-    weight = 1,
-    
-    label = ~paste0(nomDepartement, " ", nomCirconscription, ": ", round(act_cho, 2))
-  ) %>% 
-  addTiles(urlTemplate="",options = providerTileOptions(minZoom=5,maxZoom=11)) %>% 
-  setMapWidgetStyle(style = list(background="transparent")) #avoir un fond blanc
-
-
-
-# Garder geometry.x et renommer en geometry
-dtaf2 <- dtaf2 %>%
-  select(-geometry.y) %>%
-  rename(geometry = geometry.x) %>%
-  st_as_sf()  # s'assurer que c'est bien un objet sf
-
+    ) %>%
+    addLegend(
+      pal = pal, 
+      values = ~dtaf2[[input$select]],
+      title = "Pourcentage (%)",
+      position = "bottomright",
+      opacity = 0.7
+    ) %>%
+    setView(lng = 2.5, lat = 46.5, zoom = 6) %>%
+    clearTiles()
+     })
--- Data_Viz_JO/UI/Ui_carte.R
+++ Data_Viz_JO/UI/Ui_carte.R
@@ -1,6 +1,11 @@
 library(shiny)
 tagList(fluidPage(
+  selectInput( 
+    "select", 
+    "Choissisez une variable à afficher sur la carte:", 
+    list("Chomâge" = "actcho", "% de propriétaire" = "proprio", "% agriculteur" = "act_agr") 
+  ), 
   leafletOutput("mymap"),
   p(),
-  actionButton("recalc", "New points")
+ 
 ))
