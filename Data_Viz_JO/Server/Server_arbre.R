efed35724c3a99a7978eea5101f2e85d9703da06
Maéva MONTIER <maeva.montier76@gmail.com>
2025-10-06 16:54:10 +0200

    codage variable pour l'affichage du dt

--- Data_Viz_JO/Server/Server_arbre.R
+++ Data_Viz_JO/Server/Server_arbre.R
@@ -30,7 +30,7 @@ label_variable <- c(
 )
 
 # Sélection et renommage des colonnes
-dtaf_clean <- dtaf %>%
+dtaf_tree <- dtaf %>%
   select(Inscrit_22,pop_légal_19,pop_pole_aav,pop_cour_aav,pop_horsaav,pop_urb,pop_rur_periu,pop_rur_non_periu,
          age_moyen,actemp,actcho,inactret,actdip_PEU,actdip_CAP,actdip_BAC,actdip_BAC2,actdip_BAC3,actdip_BAC5,
          actdip_BAC3P,act_agr,act_art,act_cad,act_int,act_emp,act_ouv,act_cho,men_seul,men_coupae,men_coupse,
@@ -40,10 +40,10 @@ dtaf_clean <- dtaf %>%
   st_drop_geometry()
 
 # Renommer les colonnes avec les labels
-names(dtaf_clean) <- label_variable[names(dtaf_clean)]
+names(dtaf_tree) <- label_variable[names(dtaf_tree)]
 
 # Construire l’arbre
-res <- rpart(`Vote majoritaire` ~ ., data = dtaf_clean)
+res <- rpart(`Vote majoritaire` ~ ., data = dtaf_tree)
 
 output$tree <- renderVisNetwork({
   visTree(res, main = "Vote", width = "100%", height = "100vh")
@@ -53,7 +53,7 @@ output$tree <- renderVisNetwork({
 output$tree_cand <- renderVisNetwork({
   
   #arbre candidat
-  dtaf_clean_cand <- dtaf %>%
+  dtaf_tree_cand <- dtaf %>%
     select(Inscrit_22,pop_légal_19,pop_pole_aav,pop_cour_aav,pop_horsaav,pop_urb,pop_rur_periu,pop_rur_non_periu,
            age_moyen,actemp,actcho,inactret,actdip_PEU,actdip_CAP,actdip_BAC,actdip_BAC2,actdip_BAC3,actdip_BAC5,
            actdip_BAC3P,act_agr,act_art,act_cad,act_int,act_emp,act_ouv,act_cho,men_seul,men_coupae,men_coupse,
@@ -63,11 +63,11 @@ output$tree_cand <- renderVisNetwork({
     st_drop_geometry()
   
   # Renommer les colonnes avec les labels
-  names(dtaf_clean_cand) <- c(
-    label_variable[names(dtaf_clean_cand)[names(dtaf_clean_cand) != input$cand]],
+  names(dtaf_tree_cand) <- c(
+    label_variable[names(dtaf_tree_cand)[names(dtaf_tree_cand) != input$cand]],
     input$cand)
   
-  res_cand <- rpart(as.formula(paste(input$cand, "~ .")),data = dtaf_clean_cand)
+  res_cand <- rpart(as.formula(paste(input$cand, "~ .")),data = dtaf_tree_cand)
   visTree(res_cand, main = paste0("Votes pour ", names(which(c(
     "Nathalie Arthaud" = "Arthaud_exp",
     "Fabrice Roussel" = "Roussel_exp",
--- Data_Viz_JO/Server/Server_data_summ.R
+++ Data_Viz_JO/Server/Server_data_summ.R
@@ -19,19 +19,92 @@ library(summarytools)
   #   "Votes" = c("Abs_insc","Vot_insc","Blanc_vote","Nul_vote","Arthaud_exp","Roussel_exp","Macron_exp","Lassalle_exp","LePen_exp","Zemmour_exp",
   #               "Melenchon_exp","Hidalgo_exp","Jadot_exp","Pecresse_exp","Poutou_exp","DupontAignan_exp","Gagnant")
   # )
-  dtaf_summary <- dtaf %>%
-    st_drop_geometry()
-  
+dtaf_dt <- dtaf %>%
+  st_drop_geometry() %>%
+  rename(
+    "Code du département" = codeDepartement,
+    "Nom du département" = nomDepartement,
+    "Code de la circonscription" = codeCirconscription,
+    "Nom de la circonscription" = nomCirconscription,
+    "Inscrits 2022" = Inscrit_22,
+    "Population légale 2019" = pop_légal_19,
+    "% pop. pôles AAV" = pop_pole_aav,
+    "% pop. couronnes AAV" = pop_cour_aav,
+    "% pop. hors AAV" = pop_horsaav,
+    "% pop. urbaine" = pop_urb,
+    "% pop. rurale périurbaine" = pop_rur_periu,
+    "% pop. rurale non périurbaine" = pop_rur_non_periu,
+    "Âge moyen" = age_moyen,
+    "% actifs en emploi" = actemp,
+    "% chômeurs" = actcho,
+    "% inactifs retraités" = inactret,
+    "Sans diplôme" = actdip_PEU,
+    "CAP-BEP" = actdip_CAP,
+    "BAC" = actdip_BAC,
+    "Bac+2" = actdip_BAC2,
+    "Bac+3" = actdip_BAC3,
+    "Bac+5" = actdip_BAC5,
+    "Bac+3+" = actdip_BAC3P,
+    "% agriculteurs" = act_agr,
+    "% artisans/com." = act_art,
+    "% cadres" = act_cad,
+    "% prof. intermédiaires" = act_int,
+    "% employés" = act_emp,
+    "% ouvriers" = act_ouv,
+    "% chômage" = act_cho,
+    "% ménages seuls" = men_seul,
+    "% couples avec enfants" = men_coupae,
+    "% couples sans enfants" = men_coupse,
+    "% familles monoparentales" = men_monop,
+    "% propriétaires" = proprio,
+    "% locataires" = locatai,
+    "% chauffage fioul" = mfuel,
+    "% aucun transport" = modtrans_aucun,
+    "% à pied" = modtrans_pied,
+    "% vélo" = modtrans_velo,
+    "% moto" = modtrans_moto,
+    "% voiture" = modtrans_voit,
+    "% transports en commun" = modtrans_commun,
+    "% accès école" = acc_ecole,
+    "% accès collège" = acc_college,
+    "% accès lycée" = acc_lycee,
+    "% accès médecin" = acc_medecin,
+    "% accès dentiste" = acc_dentiste,
+    "% accès pharmacie" = acc_pharmacie,
+    "Taux de pauvreté (60%)" = tx_pauvrete60_diff,
+    "Niveau de vie médian" = nivvie_median_diff,
+    "Vote majoritaire" = Gagnant,
+    "Taux d'abstension" = Abs_insc,
+    "Taux de vote" = Vot_insc,
+    "% Votes blans" = Blanc_vote,
+    "% Votes nuls" = Nul_vote,
+    "Votes exprimés pour Arthaud" = Arthaud_exp,
+    "Votes exprimés pour Roussel" = Roussel_exp,
+    "Votes exprimés pour Macron" = Macron_exp,
+    "Votes exprimés pour Lasalle" = Lassalle_exp,
+    "Votes exprimés pour Le Pen" = LePen_exp,
+    "Votes exprimés pour Zemmour" = Zemmour_exp,
+    "Votes exprimés pour Mélenchon" = Melenchon_exp,
+    "Votes exprimés pour Hidalgo" = Hidalgo_exp,
+    "Votes exprimés pour Jadot" = Jadot_exp,
+    "Votes exprimés pour Pécresse" = Pecresse_exp,
+    "Votes exprimés pour Poutou" = Poutou_exp,
+    "Votes exprimés pour Dupont-Aignan" = DupontAignan_exp
+  )
+
+  # dtaf_summary <- dtaf %>%
+  #   st_drop_geometry()
+  # names(dtaf_summary) <- label_variable_df[names(dtaf_summary)]
   # Table affichée
   output$table <- renderDT({
-    DT::datatable(dtaf, options = list(pageLength = 10))
+    DT::datatable(dtaf_dt, options = list(pageLength = 10))
   })
   
   # Résumé des variables
   library(stargazer)
   
   output$summary <- renderUI({
-    df <- as.data.frame(summary(dtaf_summary[, input$select_summary, drop = FALSE]))
+    df <- as.data.frame(summary(dtaf_dt[, input$select_summary, drop = FALSE]))
     HTML(
       stargazer(df, type = "html", summary = FALSE, title = "Résumé statistique")
     )
